12df06ee2475fd77a6c6a7c537f997941dc99514
perficient1 <150013026+perficient1@users.noreply.github.com>
2023-11-06 16:05:25 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,6 +1,7 @@
 import streamlit
 streamlit.title('My Parents New Healty Diner')
 streamlit.header('Breakfast Menu')
-streamlit.text('Omega 3 & Blueberry Oatmeal')
-streamlit.text('Kale, Spinach & Smothie')
-streamlit.text('Hard Boiled Free-Range Egg')
+streamlit.text(' 🥣 Omega 3 & Blueberry Oatmeal')
+streamlit.text(' 🥗 Kale, Spinach & Smothie')
+streamlit.text(' 🐔 Hard Boiled Free-Range Egg')
+streamlit.text('🥑🍞 Avacado Toast')
